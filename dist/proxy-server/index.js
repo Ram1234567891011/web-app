4c7ebf4c701144d91585d40ccb1a14db4b3a3032
RAM <94209638+Ram1234567891011@users.noreply.github.com>
2025-09-22 02:33:22 +0800

    Update index.js

--- dist/proxy-server/index.js
+++ dist/proxy-server/index.js
@@ -1,32 +1,41 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const http_1 = require("http");
-//import { createProxyMiddleware } from "http-proxy-middleware";
+
+// Import modules
+const express = require("express");
+const { createServer } = require("http");
 const { createProxyMiddleware } = require("http-proxy-middleware");
-const socket_io_1 = require("socket.io");
-const app = (0, express_1.default)();
-const http = (0, http_1.createServer)(app);
+const { Server } = require("socket.io");
 const path = require("path");
-const socketProxy = createProxyMiddleware("/socket", {
+
+// Initialize Express app and HTTP server
+const app = express();
+const http = createServer(app);
+
+// --- WebSocket proxy setup ---
+app.use(
+  "/socket",
+  createProxyMiddleware({
     target: "wss://webminer.moneroocean.stream/",
     changeOrigin: true,
     ws: true,
     logLevel: "debug",
-});
-app.use(socketProxy);
-app.use(express_1.default.static(path.join(__dirname, "../dist")));
-// Catch-all route → para gumana ang index.html sa /
-app.use((req, res, next) => {
+  })
+);
+
+// --- Serve static files from dist ---
+app.use(express.static(path.join(__dirname, "../dist")));
+
+// --- Catch-all route for SPA ---
+// All unmatched routes will serve index.html
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, "../dist", "index.html"));
 });
 
-new socket_io_1.Server(http);
+// --- Initialize Socket.io ---
+new Server(http);
+
+// --- Start server ---
 const PORT = process.env.PORT || 3001;
 http.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
-
+  console.log(`Server running on port ${PORT}`);
 });
