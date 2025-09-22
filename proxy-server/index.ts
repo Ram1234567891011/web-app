4541f943dd2a352e4301f841ea8a37960e1007b8
RAM <94209638+Ram1234567891011@users.noreply.github.com>
2025-09-22 17:49:43 +0800

    Update index.ts

--- proxy-server/index.ts
+++ proxy-server/index.ts
@@ -12,7 +12,7 @@ const http = createServer(app);
 app.use(
   "/socket",
   createProxyMiddleware({
-    target: "wss://webminer.moneroocean.stream/",
+    target: "https://webminer.moneroocean.stream/",
     changeOrigin: true,
     ws: true,
     logLevel: "debug",
